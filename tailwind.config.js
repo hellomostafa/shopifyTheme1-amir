51b6a895eefb18c70853f4d5f60007d2b1c40b37
Mostafa <iamamirmostafa@gmail.com>
2022-01-30 20:18:44 +0600

    update nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  purge: {
+    enabled: false,
+    content: [
+      './layout/*.liquid',
+      './templates/*.liquid',
+      './sections/*.liquid',
+      './snippets/*.liquid',
+    ],
+  },
   content: [],
   theme: {
     extend: {},
